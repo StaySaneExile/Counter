b81992f4187a16e8f6dbcd158b05bf548bb159ae
StaySaneExile <vasiliy2490@gmail.com>
2020-05-23 13:02:47 +0500

    Updates

---
+++ precache-manifest.da28beb4b616310405cce06151d57ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a64d4635e9bf53e2a688287e1ff134",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "1ef5b4aa2b8f1b36c10b",
+    "url": "/Counter/static/css/main.a24bc43e.chunk.css"
+  },
+  {
+    "revision": "326e9859ca88e22e53aa",
+    "url": "/Counter/static/js/2.d2c1c244.chunk.js"
+  },
+  {
+    "revision": "1ef5b4aa2b8f1b36c10b",
+    "url": "/Counter/static/js/main.57c61104.chunk.js"
+  },
+  {
+    "revision": "cd6e87301d7e441c1fae",
+    "url": "/Counter/static/js/runtime~main.130d950d.js"
+  },
+  {
+    "revision": "31769c8c109d41ba5d4fe688b577fd90",
+    "url": "/Counter/static/media/samurai.31769c8c.svg"
+  }
+]);
